db17238d999c57ec233db246e7683df6dfebf330
Jose Antonio Hernandez <ahernandez@nearsoft.com>
2018-08-31 14:02:32 -0500

    Send message every second and print it

--- src/main/java/jahv/test/MyRouteBuilder.java
+++ src/main/java/jahv/test/MyRouteBuilder.java
@@ -1,6 +1,17 @@
 package jahv.test;
 
+import org.apache.camel.CamelContext;
+import org.apache.camel.Consumer;
+import org.apache.camel.Endpoint;
+import org.apache.camel.EndpointConfiguration;
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.PollingConsumer;
+import org.apache.camel.Processor;
+import org.apache.camel.Producer;
 import org.apache.camel.builder.RouteBuilder;
+
+import java.util.Map;
 
 /**
  * A Camel Java DSL Router
@@ -26,7 +37,15 @@ public class MyRouteBuilder extends RouteBuilder {
 
 
         from("timer:simple?period=1000")
-                .log("Processing...")
+                .log("Sending message...")
+                .setHeader("Header1", constant("HeaderValue"))
+                .setBody(constant("Input Message"))
+                .to("direct:processMessage")
+                .end();
+
+        from("direct:processMessage")
+                .log("Init message processing")
+                .log("Processing: ${body}, with header: ${header.Header1}")
                 .end();
     }
 
